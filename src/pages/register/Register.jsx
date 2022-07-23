cc4d285e8a0db8aaa012eb153d540593fcde4ea9
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2022-07-24 04:25:36 +0530

    login-context-api

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,30 +1,83 @@
-import './register.css'
+import "./register.css";
 
-import React from 'react'
+import React, { useRef } from "react";
+import axios from "axios";
+import {useHistory} from 'react-router'
 
-export default function Login() {
+
+
+export default function Register() {
+  const username = useRef();
+  const email = useRef();
+  const password = useRef();
+  const passwordAgain = useRef();
+  const history= useHistory();
+
+  const handleClick =async (e)=>{
+    e.preventDefault();
+    if(passwordAgain.current.value !== password.current.value){
+      password.current.setCustomValidity("Passwords do not match");
+    }else{
+      const user ={
+        username : username.current.value,
+        email: email.current.value,
+        password: password.current.value,
+      }
+      try{
+         await axios.post("/auth/register",user);
+         history.push("/login")
+
+      }catch(e){
+        console.log(e)
+      }
+     
+    }
+  }
+
   return (
     <div className="login">
-    <div className="loginWrapper">
-      <div className="loginLeft">
-        <h3 className="loginLogo">Facebook</h3>
-        <span className="loginDesc">
-          Connect with friends and the world around you on notFacebook.
-        </span>
-      </div>
-      <div className="loginRight">
-        <div className="loginBox">
-        <input placeholder="Username" className="loginInput" />
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
-            <input placeholder="Password Again" className="loginInput" />
-          <button className="loginButton">Sign Up</button>
-          <button className="loginRegisterButton">
-            Log into Account
-          </button>
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Facebook</h3>
+          <span className="loginDesc">
+            Connect with friends and the world around you on notFacebook.
+          </span>
+        </div>
+        <div className="loginRight">
+          <form className="loginBox" onSubmit={handleClick}>
+            <input
+              placeholder="Username"
+              required
+              ref={username}
+              className="loginInput"
+            />
+            <input
+              placeholder="Email"
+              required
+              ref={email}
+              type="email"
+              className="loginInput"
+            />
+            <input
+              placeholder="Password"
+              required
+              type='password'
+              ref={password}
+              minLength='6'
+              className="loginInput"
+            />
+            <input
+              placeholder="Password Again"
+              required
+              type='password'
+              ref={passwordAgain}
+              className="loginInput"
+            />
+            <button className="loginButton" type="submit">Sign Up</button>
+            <button className="loginRegisterButton">Log into Account</button>
+          </form>
         </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
