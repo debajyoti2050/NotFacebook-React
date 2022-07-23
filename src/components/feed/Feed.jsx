43fd1f1b76835de7f6a1cb6dc92bffb85d10ba03
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2022-07-23 13:27:09 +0530

    apiv1

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,16 +1,38 @@
 import "./feed.css";
+import {useState} from "react";
 import Share from "../share/Share";
 import Post from "../post/Post";
-import {Posts} from '../../dummyData'
+import { useEffect } from "react";
+import axios from "axios";
+// import {Posts} from '../../dummyData'
 
-export default function Feed() {
+export default function Feed({username}) {
+  const [posts,setPosts]= useState([])
+
+  useEffect(()=>{
+    const fetchPosts= async()=>{
+      const res= username
+                 ?await axios.get("/posts/profile/"+ username)
+                 : await axios.get("posts/timeline/627440ff4be1a337727ef8f9");
+      setPosts(res.data)
+    }
+    fetchPosts();
+
+  },[username]);
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share/>
-        {Posts.map(p=>(
+        {/* {Posts.map(p=>(
           <Post key={p.id} post={p}/>
-        ))}
+        ))} */}
+
+        {
+        posts.map(p=>(
+          <Post key={p._id} post={p}/>
+        ))
+        }
+
       </div>
     </div>
   )
